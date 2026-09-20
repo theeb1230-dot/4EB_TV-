c7cef701f8a357758e7d3cedb7b0e165417be87d
Arab_Theeb <theeb1230@gmail.com>
2026-09-20 15:41:58 +0300

    fix(playback): implement provider download contract in test double

--- packages/playback_orchestrator/test/playback_orchestrator_test.dart
+++ packages/playback_orchestrator/test/playback_orchestrator_test.dart
@@ -20,6 +20,9 @@ final class FakeProvider implements Provider {
 
   @override
   Future<ResolveResult> resolve(ResolveRequest request) async => result;
+
+  @override
+  Future<Uri?> resolveDownload(ResolveRequest request) async => null;
 }
 
 void main() {
