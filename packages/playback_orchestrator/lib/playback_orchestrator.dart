55bd00b545c0992c2eb0da5320fa881767e2f884
Arab_Theeb <theeb1230@gmail.com>
2026-09-20 17:11:22 +0300

    style(orchestrator): apply canonical Dart formatting

--- packages/playback_orchestrator/lib/playback_orchestrator.dart
+++ packages/playback_orchestrator/lib/playback_orchestrator.dart
@@ -1,4 +1,5 @@
 library playback_orchestrator;
 
 export 'src/playback_orchestrator.dart';
-export 'package:resolver_engine/resolver_engine.dart' show AttemptDisposition, AttemptResult;
+export 'package:resolver_engine/resolver_engine.dart'
+    show AttemptDisposition, AttemptResult;
