ede81ab43e0e7fb0332fe37d65513ca5e2e24dd8
Arab_Theeb <theeb1230@gmail.com>
2026-09-20 17:04:13 +0300

    fix(player): expose attempt disposition with result contract

--- packages/playback_orchestrator/lib/playback_orchestrator.dart
+++ packages/playback_orchestrator/lib/playback_orchestrator.dart
@@ -1,4 +1,4 @@
 library playback_orchestrator;
 
 export 'src/playback_orchestrator.dart';
-export 'package:resolver_engine/resolver_engine.dart' show AttemptResult;
+export 'package:resolver_engine/resolver_engine.dart' show AttemptDisposition, AttemptResult;
