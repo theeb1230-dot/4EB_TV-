42e730c7e0a4067e3966fed2f6cefb752939bf5f
Arab_Theeb <theeb1230@gmail.com>
2026-09-24 14:09:41 +0300

    style: apply dart formatter output to watch progress builder test

--- packages/flutter_presentation/test/watch_progress_sections_builder_test.dart
+++ packages/flutter_presentation/test/watch_progress_sections_builder_test.dart
@@ -36,8 +36,16 @@ void main() {
     ]);
     await tester.pump();
 
-    expect(find.byKey(const ValueKey('watch-progress-item-continue-watching-runtime-title')), findsOneWidget);
-    expect(find.byKey(const ValueKey('watch-progress-item-history-runtime-title')), findsOneWidget);
+    expect(
+      find.byKey(
+        const ValueKey('watch-progress-item-continue-watching-runtime-title'),
+      ),
+      findsOneWidget,
+    );
+    expect(
+      find.byKey(const ValueKey('watch-progress-item-history-runtime-title')),
+      findsOneWidget,
+    );
     expect(find.text('runtime-title'), findsNWidgets(2));
     expect(find.text('S1 E2 • 25%'), findsNWidgets(2));
 
