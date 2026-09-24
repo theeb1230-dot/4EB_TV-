67ec1824b8b2a1246ee322b29f94e72ff2a1a0d0
Arab_Theeb <theeb1230@gmail.com>
2026-09-25 00:10:16 +0300

    fix: remove unused local persistence import

--- packages/local_data_flutter/lib/watch_progress_local_runtime.dart
+++ packages/local_data_flutter/lib/watch_progress_local_runtime.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:core_domain/core_domain.dart';
-import 'package:local_data_persistent/local_data_persistent.dart';
 import 'package:presentation_contract/presentation_contract.dart';
 
 const _watchProgressKey = 'watch-progress-v1';
